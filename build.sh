e1caedd89fb4a3e23977c1f1cc6073eb543d6bb9
Johnathan Coe <jcoe148@aucklanduni.ac.nz>
2021-08-30 19:11:15 +1200

    docs: Log build process

--- build.sh
+++ build.sh
@@ -4,13 +4,18 @@
 rm -r target
 
 # Compile moon files into target/moon
-moonc src/main/moon --output-to target
+printf "Compiling MoonScript..."
+time moonc src/main/moon --output-to target
+echo ""
 
 # target/moon -> target/JJHome.lkt
 mv target/moon target/JJHome.lkt
 
+echo "Copying resources into 'target'..."
 # Copy lua files
 cp -r src/main/lua/* target/JJHome.lkt
 
 # Copy resources (plugin.yml etc.)
 cp -r src/main/resources/* target/JJHome.lkt
+
+echo "Built target/JJHome.lkt!"
